a27ca7b1d3139acd241b9da91817e7be8163c89f
stev12O <stev12O@users.noreply.github.com>
2026-06-12 18:15:37 -0400

    clean: remove get_source.php

--- get_source.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "=== PROXY.PHP ===\n";
-echo file_get_contents('proxy.php');
-echo "\n=== SCRIPT.JS ===\n";
-echo file_get_contents('script.js');
-?>
